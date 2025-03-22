8ce5d16b2a8450f68e3d375e859208cc624aacd5
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-03-22 23:22:42 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,6 @@
-
+from .admin_handler import *
+from .ai_chat_handler import *
+from .auth_handler import *
+from .effects_handler import *
+from .games_handler import *
+from .music_handler import *
