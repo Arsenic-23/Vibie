ad4aa5a654e25a3a224bcc810ff6bec9f54393aa
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-04 02:04:50 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,19 @@
-b
+class SongQueue:
+    def __init__(self):
+        self.queue = []
+    
+    def add_song(self, song_name, song_url):
+        self.queue.append({"name": song_name, "url": song_url})
+    
+    def next_song(self):
+        if self.queue:
+            return self.queue.pop(0)
+        return None
+    
+    def get_queue(self):
+        return self.queue
+    
+    def clear(self):
+        self.queue = []
+
+queue = SongQueue()
