964f5c38459df6b4694e9d006ca013cfd45d8284
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-04 17:30:00 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("7575567291:AAH8yaPhRofVfAFrDPiEqddelnqszgQ5Ru8")
 BACKEND_API_URL = os.getenv("BACKEND_API_URL")
